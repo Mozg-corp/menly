20f2b32002d8fead35c681f72998f4b63ef1622a
mozg3000 <i.luka@outlook.com>
2020-10-19 17:36:39 +0300

    fix email sending on Profile creation

--- bootstrap/DependencyInjector.php
+++ bootstrap/DependencyInjector.php
@@ -3,5 +3,7 @@ class DependencyInjector implements \yii\base\BootstrapInterface{
 	public function bootstrap ($app){
 		// \Yii::$container->set(\app\interfaces\ClientInterface::class, ['class' => '\app\services\HttpClientService']);
 		// \Yii::$container->set(\app\services\LoginService::class, [], [\Yii::$container->get(\app\interfaces\ClientInterface::class)]);
+		// \Yii::CreateObject(['class'=>\app\components\NotificationComponent::class, 'mailer' => \Yii::$app->mailer]);
+		//\Yii::$container->set(\app\services\LoginService::class, [], [\Yii::$container->get(\app\interfaces\ClientInterface::class)]);
 	}
 }
--- components/NotificationComponent.php
+++ components/NotificationComponent.php
@@ -3,23 +3,27 @@ namespace app\components;
 use yii\base\Component;
 
 class NotificationComponent extends Component{
-	public $mailer;
-	public function sendProfileCreatedNotification($profile){
-		$respond = $this->mailer->compose('profile_created', ['profile' => $profile])
-					->setSubject('New profile created')
-					->setFrom('menly@iluka.ru')
-					->setTo('voiptek@yandex.ru')
-					->send();
+	public $mailSevice;
+	public function __construct($config=[]){
+		$this->mailSevice = \Yii::$app->mailer;
+		parent::__construct($config);
 	}
+	// public function sendProfileCreatedNotification($profile){
+		// $respond = $this->mailSevice->compose('profile_created', ['profile' => $profile])
+					// ->setSubject('New profile created')
+					// ->setFrom('menly@iluka.ru')
+					// ->setTo('voiptek@yandex.ru')
+					// ->send();
+	// }
 	public function sendProfileCreatedNotification($event){
-		$respond = $this->mailer->compose('profile_created', ['profile' => $event])
+		$respond = $this->mailSevice->compose('profile_created', ['profile' => $event])
 					->setSubject('New profile created')
 					->setFrom('menly@iluka.ru')
 					->setTo('voiptek@yandex.ru')
 					->send();
 	}
 	public function sendCarCreatedNotification($event){
-		$respond = $this->mailer->compose('car_created', ['car' => $event])
+		$respond = $this->mailSevice->compose('car_created', ['car' => $event])
 					->setSubject('New car created')
 					->setFrom('menly@iluka.ru')
 					->setTo('voiptek@yandex.ru')
