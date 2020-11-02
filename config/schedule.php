98c7cff8bcf1274ef360e0ec123e14adbd57de46
mozg3000 <i.luka@outlook.com>
2020-11-02 17:29:27 +0300

    adjust scheduler

--- config/schedule.php
+++ config/schedule.php
@@ -3,7 +3,7 @@
  * @var \omnilight\scheduling\Schedule $schedule
  */
  // echo 'tast job';
- $schedule->command('balance/gett-report')->hourly();//->sendOutputTo('@app/log/application.log');;
+$schedule->command('balance/gett-report')->hourly()->sendOutputTo('/data/menly/raw/cron.log');
  // $schedule->call(function(\yii\console\Application $app) {
     //Some code here...
 	// echo 'ok';
