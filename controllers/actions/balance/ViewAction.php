f53c21ea4f349c461f20e1454c803105a14f03e3
mozg3000 <i.luka@outlook.com>
2020-11-06 16:18:53 +0300

    cleaned up commented code

--- controllers/actions/balance/ViewAction.php
+++ controllers/actions/balance/ViewAction.php
@@ -20,48 +20,5 @@ class ViewAction extends \yii\rest\ViewAction{
 		}else{
 			throw new \yii\web\NotFoundHttpException('Не найдены зарегистрированные аккаунты агрегаторов');
 		}
-		/*								
-		$promises = [];
-		forEach($driverAccounts as $account){
-			$name = $account['name'];
-			$driver_uid = $account['account'];
-			$promises[$name] = $this->controller->client->getBalanceByName($name, $driver_uid);			
-		}
-		try{
-			$responses = \GuzzleHttp\Promise\settle($promises)->wait();
-		}catch(\GuzzleHttp\Exception\ClientException $e){
-			$response = $e->getResponse();
-			$responseBodyAsString = $response->getBody()->getContents();
-			$responseStdObj = json_decode($responseBodyAsString);
-			return $responseBodyAsString;
-		}
-		$balances = [];
-		forEach($driverAccounts as $account){
-			$name = $account['name'];
-			$staticService = $this->controller->factory->getClassByName($name);
-			$statePromise = $responses[$name]['state'];
-			
-			if($statePromise === 'fulfilled'){
-				$balances[$name] = $staticService::extractBalanceFromResponse($responses[$name]);
-			}else if($statePromise === 'rejected'){
-				$rawReasonBody = $responses[$name]['reason']->getResponse()->getBody()->getContents();
-				$reasonBody = json_decode($rawReasonBody);
-				if($reasonBody->code === 401 && $reasonBody->message === 'Expired JWT Token'){
-					$agregator = \app\models\Agregator::find()->getAgregatorByName($name);
-					$agregator->flushToken();
-					$driver_uid = $account['account'];
-					$promise = $this->controller->client->getBalanceByName($name, $driver_uid);
-					$response = $promise->wait();
-					$body = $response->getBody()->getContents();
-					$balances[$name] = $staticService::extractBalanceFromBody(json_decode($body));
-
-				}
-			}
-			// print_r($responses[$name]['reason']->getResponse()->getBody()->getContents());
-
-			
-		}
-		return $balances;
-		//*/
 	}
 }
