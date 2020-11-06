876996de355f914ae01dddc43b79b1f26b56088d
mozg3000 <i.luka@outlook.com>
2020-11-06 16:15:46 +0300

    just cleaned up comments

--- controllers/actions/useragregator/BatchAction.php
+++ controllers/actions/useragregator/BatchAction.php
@@ -1,22 +1,15 @@
 <?php namespace app\controllers\actions\useragregator;
 class BatchAction extends \yii\rest\Action{
 	public function run(){
-        // if ($this->checkAccess) {
-            // call_user_func($this->checkAccess, $this->id, $profile);
-        // }
-		// print_r(\Yii::$app->getRequest()->getBodyParams()); die;
 		$relations = \Yii::$app->getRequest()->getBodyParams();
 		$models = [];
-		// return $this->modelClass();die;
 		$user = \Yii::$app->user->identity;
 		$userAgregators = $user->agregators;
-		//return $userAgregators;die;
 		if(!count($userAgregators)){
 			forEach($relations as $uaRelation){
 				
 					$model = new $this->controller->modelClass();
 					$model->load($uaRelation, '');
-					//$model->scenarioBatchCreate();
 					if(!$model->save()){
 						return $model->errors;
 					}
